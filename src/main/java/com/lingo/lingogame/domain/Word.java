d54f3c24bdced60947c09e75e1a12bec01310707
MaxRaadgever <maxraadgever@gmail.com>
2020-05-19 17:39:18 +0200

    Changed word validator to use predicate list

--- src/main/java/com/lingo/lingogame/domain/Word.java
+++ src/main/java/com/lingo/lingogame/domain/Word.java
@@ -2,10 +2,17 @@ package com.lingo.lingogame.domain;
 
 import javax.persistence.*;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
+import java.util.function.Predicate;
 
 @Entity
 public class Word {
+
+    private static final int MIN_LENGTH = 5;
+    private static final int MAX_LENGTH = 7;
+    private static final String ALLOWED_REGEX = "[a-z]+\\.?";
 
     @Id
     @Column(unique = true)
@@ -17,15 +24,20 @@ public class Word {
     @OneToMany(fetch = FetchType.EAGER, mappedBy = "correctWord")
     private Set<Game> games = new HashSet<>();
 
+    @Transient
+    private final List<Predicate<Word>> validators = new LinkedList<>();
+
     public Word(String word, String language) {
         this.word = word;
         this.language = language;
         this.length = word.length();
+        initializeValidators();
     }
 
     public Word(String word) {
         this.word = word;
         this.length = word.length();
+        initializeValidators();
     }
 
     public Word() {}
@@ -52,18 +64,24 @@ public class Word {
     }
 
     public boolean isValid() {
-        if(!this.word.matches("[a-z]+\\.?")) {
-            return false;
-        }
-
-        if (this.length < 5 || this.length > 7) {
-            return false;
-        }
-
-        return true;
+        return validators.stream().allMatch(wordPredicate -> wordPredicate.test(this));
     }
 
     public Set<Game> getGames() {
         return games;
     }
+
+    private void initializeValidators() {
+        this.validators.add(Word::isBetweenAllowedLength);
+        this.validators.add(Word::isAllowedCharacters);
+    }
+
+    private static boolean isBetweenAllowedLength(Word word){
+        return MIN_LENGTH <= word.getLength() && word.getLength() <= MAX_LENGTH;
+    }
+
+    private static boolean isAllowedCharacters(Word word) {
+        return word.getWord().matches(ALLOWED_REGEX);
+    }
+
 }
